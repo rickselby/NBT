25bd77289c85f9780959a6c7ba1ceac23051efd7
Rick Selby <rick@selby-family.co.uk>
2015-10-20 22:30:07 +0100

    Move data functions out of the Service class ready for testing

---
+++ lib/Nbt/DataHandler.php
@@ -0,0 +1,374 @@
+<?php
+
+namespace Nbt;
+
+class DataHandler
+{
+    /**
+     * Read a byte tag from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return byte
+     */
+    public static function getTAGByte($fPtr)
+    {
+        return unpack('c', fread($fPtr, 1))[1];
+    }
+
+    /**
+     * Write a byte tag to the file.
+     *
+     * @param resource $fPtr
+     * @param byte     $byte
+     *
+     * @return bool
+     */
+    public static function putTAGByte($fPtr, $byte)
+    {
+        return is_int(fwrite($fPtr, pack('c', $byte)));
+    }
+
+    /**
+     * Read a string from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return string
+     */
+    public static function getTAGString($fPtr)
+    {
+        if (!$stringLength = self::getTAGShort($fPtr)) {
+            return '';
+        }
+        // Read in number of bytes specified by string length, and decode from utf8.
+        return utf8_decode(fread($fPtr, $stringLength));
+    }
+
+    /**
+     * Write a string to the file.
+     *
+     * @param resource $fPtr
+     * @param string   $string
+     *
+     * @return bool
+     */
+    public static function putTAGString($fPtr, $string)
+    {
+        $value = utf8_encode($string);
+
+        return self::putTAGShort($fPtr, strlen($value))
+                && is_int(fwrite($fPtr, $value));
+    }
+
+    /**
+     * Read a short int from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return int
+     */
+    public static function getTAGShort($fPtr)
+    {
+        return self::unsignedToSigned(unpack('n', fread($fPtr, 2))[1], 16);
+    }
+
+    /**
+     * Write a short int to the file.
+     *
+     * @param resource $fPtr
+     * @param int      $short
+     *
+     * @return bool
+     */
+    public static function putTAGShort($fPtr, $short)
+    {
+        return is_int(fwrite($fPtr, pack('n', self::signedToUnsigned($short, 16))));
+    }
+
+    /**
+     * Get an int from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return int
+     */
+    public static function getTAGInt($fPtr)
+    {
+        return self::unsignedToSigned(unpack('N', fread($fPtr, 4))[1], 32);
+    }
+
+    /**
+     * Write an integer to the file.
+     *
+     * @param resource $fPtr
+     * @param int      $int
+     *
+     * @return bool
+     */
+    public static function putTAGInt($fPtr, $int)
+    {
+        return is_int(fwrite($fPtr, pack('N', self::signedToUnsigned($int, 32))));
+    }
+
+    /**
+     * Read a long int from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return int
+     */
+    public static function getTAGLong($fPtr)
+    {
+        list(, $firstHalf, $secondHalf) = unpack('N*', fread($fPtr, 8));
+        if (PHP_INT_SIZE >= 8) {
+            // Workaround for PHP bug #47564 in 64-bit PHP<=5.2.9
+            $firstHalf &= 0xFFFFFFFF;
+            $secondHalf &= 0xFFFFFFFF;
+
+            $value = ($firstHalf << 32) | $secondHalf;
+            if ($value > pow(2, 63)) {
+                $value -= pow(2, 64);
+            }
+        } else {
+            if (!extension_loaded('gmp')) {
+                trigger_error(
+                    'This file contains a 64-bit number and execution cannot continue. '
+                    .'Please install the GMP extension for 64-bit number handling.',
+                    E_USER_ERROR
+                );
+            }
+
+            // Fix values >= 2^31 (same fix as above, but this time because it's > PHP_INT_MAX)
+            $firstHalf = gmp_and($firstHalf, '0xFFFFFFFF');
+            $secondHalf = gmp_and($secondHalf, '0xFFFFFFFF');
+
+            $value = gmp_add($secondHalf, gmp_mul($firstHalf, '4294967296'));
+            if (gmp_cmp($value, gmp_pow(2, 63)) >= 0) {
+                $value = gmp_sub($value, gmp_pow(2, 64));
+            }
+            $value = gmp_strval($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Write a long int to the file.
+     *
+     * @param resource $fPtr
+     * @param int      $long
+     *
+     * @return bool
+     */
+    public static function putTAGLong($fPtr, $long)
+    {
+        if (PHP_INT_SIZE >= 8) {
+            $firstHalf = ($long & 0xFFFFFFFF00000000) >> 32;
+            $secondHalf = $long & 0xFFFFFFFF;
+
+            $wResult = is_int(fwrite($fPtr, pack('NN', $firstHalf, $secondHalf)));
+        } else {
+            if (!extension_loaded('gmp')) {
+                trigger_error(
+                    'This file contains a 64-bit number and execution cannot continue. '
+                    .'Please install the GMP extension for 64-bit number handling.',
+                    E_USER_ERROR
+                );
+            }
+
+            // 32-bit longs seem to be too long for pack() on 32-bit machines. Split into 4x16-bit instead.
+            $quarters[ 0 ] = gmp_div(gmp_and($long, '0xFFFF000000000000'), gmp_pow(2, 48));
+            $quarters[ 1 ] = gmp_div(gmp_and($long, '0x0000FFFF00000000'), gmp_pow(2, 32));
+            $quarters[ 2 ] = gmp_div(gmp_and($long, '0x00000000FFFF0000'), gmp_pow(2, 16));
+            $quarters[ 3 ] = gmp_and($long, '0xFFFF');
+
+            $wResult = is_int(fwrite(
+                $fPtr,
+                pack(
+                    'nnnn',
+                    gmp_intval($quarters[ 0 ]),
+                    gmp_intval($quarters[ 1 ]),
+                    gmp_intval($quarters[ 2 ]),
+                    gmp_intval($quarters[ 3 ])
+                )
+            ));
+        }
+
+        return $wResult;
+    }
+
+    /**
+     * Read a float from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return float
+     */
+    public static function getTAGFloat($fPtr)
+    {
+        list(, $value) = (pack('d', 1) == "\77\360\0\0\0\0\0\0")
+            ? unpack('f', fread($fPtr, 4))
+            : unpack('f', strrev(fread($fPtr, 4)));
+
+        return $value;
+    }
+
+    /**
+     * Write a float to the file.
+     *
+     * @param resource $fPtr
+     * @param float    $float
+     *
+     * @return bool
+     */
+    public static function putTAGFloat($fPtr, $float)
+    {
+        return is_int(
+            fwrite(
+                $fPtr,
+                (pack('d', 1) == "\77\360\0\0\0\0\0\0")
+                    ? pack('f', $float)
+                    : strrev(pack('f', $float))
+            )
+        );
+    }
+
+    /**
+     * Read a double from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return float
+     */
+    public static function getTAGDouble($fPtr)
+    {
+        list(, $value) = (pack('d', 1) == "\77\360\0\0\0\0\0\0")
+            ? unpack('d', fread($fPtr, 8))
+            : unpack('d', strrev(fread($fPtr, 8)));
+
+        return $value;
+    }
+
+    /**
+     * Write a double to the file.
+     *
+     * @param resource $fPtr
+     * @param float    $double
+     *
+     * @return bool
+     */
+    public static function putTAGDouble($fPtr, $double)
+    {
+        return is_int(
+            fwrite(
+                $fPtr,
+                (pack('d', 1) == "\77\360\0\0\0\0\0\0")
+                    ? pack('d', $double)
+                    : strrev(pack('d', $double))
+            )
+        );
+    }
+
+    /**
+     * Read an array of bytes from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return byte[]
+     */
+    public static function getTAGByteArray($fPtr)
+    {
+        $arrayLength = self::getTAGInt($fPtr);
+
+        return array_values(unpack('c*', fread($fPtr, $arrayLength)));
+    }
+
+    /**
+     * Write an array of bytes to the file.
+     *
+     * @param resource $fPtr
+     * @param byte[]   $array
+     *
+     * @return bool
+     */
+    public static function putTAGByteArray($fPtr, $array)
+    {
+        return self::putTAGInt($fPtr, count($array))
+            && is_int(fwrite(
+                $fPtr,
+                call_user_func_array(
+                    'pack',
+                    array_merge(['c'.count($array)], $array)
+                )
+            ));
+    }
+
+    /**
+     * Read an array of integers from the file.
+     *
+     * @param resource $fPtr
+     *
+     * @return int[]
+     */
+    public static function getTAGIntArray($fPtr)
+    {
+        $arrayLength = self::getTAGInt($fPtr);
+
+        return array_values(unpack('N*', fread($fPtr, $arrayLength * 4)));
+    }
+
+    /**
+     * Write an array of integers to the file.
+     *
+     * @param resource $fPtr
+     * @param int[]    $array
+     *
+     * @return bool
+     */
+    public static function putTAGIntArray($fPtr, $array)
+    {
+        return self::putTAGInt($fPtr, count($array))
+            && is_int(fwrite(
+                $fPtr,
+                call_user_func_array(
+                    'pack',
+                    array_merge(['N'.count($array)], $array)
+                )
+            ));
+    }
+
+    /**
+     * Convert an unsigned int to signed, if required.
+     *
+     * @param int $value
+     * @param int $size
+     *
+     * @return int
+     */
+    private static function unsignedToSigned($value, $size)
+    {
+        if ($value >= pow(2, $size - 1)) {
+            $value -= pow(2, $size);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Convert an unsigned int to signed, if required.
+     *
+     * @param int $value
+     * @param int $size
+     *
+     * @return int
+     */
+    private static function signedToUnsigned($value, $size)
+    {
+        if ($value < 0) {
+            $value += pow(2, $size);
+        }
+
+        return $value;
+    }
+}
--- lib/Nbt/Service.php
+++ lib/Nbt/Service.php
@@ -156,7 +156,7 @@ class Service
             return false;
         }
         // Read type byte
-        $tagType = $this->getTAGByte($fPtr);
+        $tagType = DataHandler::getTAGByte($fPtr);
         if ($tagType == Tag::TAG_END) {
             return false;
         } else {
@@ -164,7 +164,7 @@ class Service
             if ($this->verbose) {
                 $position = ftell($fPtr);
             }
-            $tagName = $this->getTAGString($fPtr);
+            $tagName = DataHandler::getTAGString($fPtr);
             if ($this->verbose) {
                 trigger_error("Reading tag \"{$tagName}\" at offset {$position}.", E_USER_NOTICE);
             }
@@ -193,8 +193,8 @@ class Service
             );
         }
 
-        return $this->putTAGByte($fPtr, $node->getType())
-            && $this->putTAGString($fPtr, $node->getName())
+        return DataHandler::putTAGByte($fPtr, $node->getType())
+            && DataHandler::putTAGString($fPtr, $node->getName())
             && $this->writeType($fPtr, $node->getType(), $node);
     }
 
@@ -211,69 +211,35 @@ class Service
     {
         switch ($tagType) {
             case Tag::TAG_BYTE: // Signed byte (8 bit)
-                $node->setValue($this->getTAGByte($fPtr));
+                $node->setValue(DataHandler::getTAGByte($fPtr));
                 break;
             case Tag::TAG_SHORT: // Signed short (16 bit, big endian)
-                $node->setValue($this->getTAGShort($fPtr));
+                $node->setValue(DataHandler::getTAGShort($fPtr));
                 break;
             case Tag::TAG_INT: // Signed integer (32 bit, big endian)
-                $node->setValue($this->getTAGInt($fPtr));
+                $node->setValue(DataHandler::getTAGInt($fPtr));
                 break;
             case Tag::TAG_LONG: // Signed long (64 bit, big endian)
-                list(, $firstHalf, $secondHalf) = unpack('N*', fread($fPtr, 8));
-                if (PHP_INT_SIZE >= 8) {
-                    // Workaround for PHP bug #47564 in 64-bit PHP<=5.2.9
-                    $firstHalf &= 0xFFFFFFFF;
-                    $secondHalf &= 0xFFFFFFFF;
-
-                    $value = ($firstHalf << 32) | $secondHalf;
-                    if ($value > pow(2, 63)) {
-                        $value -= pow(2, 64);
-                    }
-                } else {
-                    if (!extension_loaded('gmp')) {
-                        trigger_error(
-                            'This file contains a 64-bit number and execution cannot continue. '
-                            .'Please install the GMP extension for 64-bit number handling.',
-                            E_USER_ERROR
-                        );
-                    }
-
-                    // Fix values >= 2^31 (same fix as above, but this time because it's > PHP_INT_MAX)
-                    $firstHalf = gmp_and($firstHalf, '0xFFFFFFFF');
-                    $secondHalf = gmp_and($secondHalf, '0xFFFFFFFF');
-
-                    $value = gmp_add($secondHalf, gmp_mul($firstHalf, '4294967296'));
-                    if (gmp_cmp($value, gmp_pow(2, 63)) >= 0) {
-                        $value = gmp_sub($value, gmp_pow(2, 64));
-                    }
-                    $value = gmp_strval($value);
-                }
-                $node->setValue($value);
+                $node->setValue(DataHandler::getTAGLong($fPtr));
                 break;
             case Tag::TAG_FLOAT: // Floating point value (32 bit, big endian, IEEE 754-2008)
-                list(, $value) = (pack('d', 1) == "\77\360\0\0\0\0\0\0")
-                    ? unpack('f', fread($fPtr, 4))
-                    : unpack('f', strrev(fread($fPtr, 4)));
-                $node->setValue($value);
+                $node->setValue(DataHandler::getTAGFloat($fPtr));
                 break;
             case Tag::TAG_DOUBLE: // Double value (64 bit, big endian, IEEE 754-2008)
-                list(, $value) = (pack('d', 1) == "\77\360\0\0\0\0\0\0")
-                    ? unpack('d', fread($fPtr, 8))
-                    : unpack('d', strrev(fread($fPtr, 8)));
-                $node->setValue($value);
+                $node->setValue(DataHandler::getTAGDouble($fPtr));
                 break;
             case Tag::TAG_BYTE_ARRAY: // Byte array
-                $arrayLength = $this->getTAGInt($fPtr);
-                $array = array_values(unpack('c*', fread($fPtr, $arrayLength)));
-                $node->setValue($array);
+                $node->setValue(DataHandler::getTAGByteArray($fPtr));
                 break;
             case Tag::TAG_STRING: // String
-                $node->setValue($this->getTAGString($fPtr));
+                $node->setValue(DataHandler::getTAGString($fPtr));
+                break;
+            case Tag::TAG_INT_ARRAY:
+                $node->setValue(DataHandler::getTAGIntArray($fPtr));
                 break;
             case Tag::TAG_LIST: // List
-                $tagID = $this->getTAGByte($fPtr);
-                $listLength = $this->getTAGInt($fPtr);
+                $tagID = DataHandler::getTAGByte($fPtr);
+                $listLength = DataHandler::getTAGInt($fPtr);
                 if ($this->verbose) {
                     trigger_error("Reading in list of {$listLength} tags of type {$tagID}.", E_USER_NOTICE);
                 }
@@ -299,87 +265,7 @@ class Service
                     $compoundNode = new Node();
                 }
                 break;
-            case Tag::TAG_INT_ARRAY:
-                $arrayLength = $this->getTAGInt($fPtr);
-                $array = array_values(unpack('N*', fread($fPtr, $arrayLength * 4)));
-                $node->setValue($array);
-                break;
         }
-    }
-
-    /**
-     * Read a byte tag from the file.
-     *
-     * @param resource $fPtr
-     *
-     * @return byte
-     */
-    private function getTAGByte($fPtr)
-    {
-        return unpack('c', fread($fPtr, 1))[1];
-    }
-
-    /**
-     * Read a string from the file.
-     *
-     * @param resource $fPtr
-     *
-     * @return string
-     */
-    private function getTAGString($fPtr)
-    {
-        if (!$stringLength = $this->getTAGShort($fPtr)) {
-            return '';
-        }
-        // Read in number of bytes specified by string length, and decode from utf8.
-        return utf8_decode(fread($fPtr, $stringLength));
-    }
-
-    /**
-     * Read a short int from the file.
-     *
-     * @param resource $fPtr
-     *
-     * @return int
-     */
-    private function getTAGShort($fPtr)
-    {
-        return $this->unsignedToSigned(
-            unpack('n', fread($fPtr, 2))[1],
-            16
-        );
-    }
-
-    /**
-     * Get an int from the file.
-     *
-     * @param resource $fPtr
-     *
-     * @return int
-     */
-    private function getTAGInt($fPtr)
-    {
-        return $this->unsignedToSigned(
-            unpack('N', fread($fPtr, 4))[1],
-            32
-        );
-    }
-
-    /**
-     * Convert an unsigned int to signed, if required.
-     *
-     * @param int $value
-     * @param int $size
-     *
-     * @return int
-     */
-    private function unsignedToSigned($value, $size)
-    {
-        if ($value >= pow(2, $size - 1)) {
-            $value -= pow(2, $size);
-        }
-
-        return $value;
     }
 
     /**
@@ -395,71 +281,23 @@ class Service
     {
         switch ($tagType) {
             case Tag::TAG_BYTE: // Signed byte (8 bit)
-                return $this->putTAGByte($fPtr, $node->getValue());
+                return DataHandler::putTAGByte($fPtr, $node->getValue());
             case Tag::TAG_SHORT: // Signed short (16 bit, big endian)
-                return $this->putTAGShort($fPtr, $node->getValue());
+                return DataHandler::putTAGShort($fPtr, $node->getValue());
             case Tag::TAG_INT: // Signed integer (32 bit, big endian)
-                return $this->putTAGInt($fPtr, $node->getValue());
+                return DataHandler::putTAGInt($fPtr, $node->getValue());
             case Tag::TAG_LONG: // Signed long (64 bit, big endian)
-                $value = $node->getValue();
-                if (PHP_INT_SIZE >= 8) {
-                    $firstHalf = ($value & 0xFFFFFFFF00000000) >> 32;
-                    $secondHalf = $value & 0xFFFFFFFF;
-
-                    $wResult = is_int(fwrite($fPtr, pack('NN', $firstHalf, $secondHalf)));
-                } else {
-                    if (!extension_loaded('gmp')) {
-                        trigger_error(
-                            'This file contains a 64-bit number and execution cannot continue. '
-                            .'Please install the GMP extension for 64-bit number handling.',
-                            E_USER_ERROR
-                        );
-                    }
-
-                    // 32-bit values seem to be too long for pack() on 32-bit machines. Split into 4x16-bit instead.
-                    $quarters[ 0 ] = gmp_div(gmp_and($value, '0xFFFF000000000000'), gmp_pow(2, 48));
-                    $quarters[ 1 ] = gmp_div(gmp_and($value, '0x0000FFFF00000000'), gmp_pow(2, 32));
-                    $quarters[ 2 ] = gmp_div(gmp_and($value, '0x00000000FFFF0000'), gmp_pow(2, 16));
-                    $quarters[ 3 ] = gmp_and($value, '0xFFFF');
-
-                    $wResult = is_int(fwrite(
-                        $fPtr,
-                        pack(
-                            'nnnn',
-                            gmp_intval($quarters[ 0 ]),
-                            gmp_intval($quarters[ 1 ]),
-                            gmp_intval($quarters[ 2 ]),
-                            gmp_intval($quarters[ 3 ])
-                        )
-                    ));
-                }
-
-                return $wResult;
+                return DataHandler::putTAGLong($fPtr, $node->getValue());
             case Tag::TAG_FLOAT: // Floating point value (32 bit, big endian, IEEE 754-2008)
-                $value = $node->getValue();
-
-                return is_int(fwrite($fPtr, (pack('d', 1) == "\77\360\0\0\0\0\0\0")
-                    ? pack('f', $value)
-                    : strrev(pack('f', $value))));
+                return DataHandler::putTAGFloat($fPtr, $node->getValue());
             case Tag::TAG_DOUBLE: // Double value (64 bit, big endian, IEEE 754-2008)
-                $value = $node->getValue();
-
-                return is_int(fwrite($fPtr, (pack('d', 1) == "\77\360\0\0\0\0\0\0")
-                    ? pack('d', $value)
-                    : strrev(pack('d', $value))));
+                return DataHandler::putTAGDouble($fPtr, $node->getValue());
             case Tag::TAG_BYTE_ARRAY: // Byte array
-                $value = $node->getValue();
-
-                return $this->putTAGInt($fPtr, count($value))
-                    && is_int(fwrite(
-                        $fPtr,
-                        call_user_func_array(
-                            'pack',
-                            array_merge(['c'.count($value)], $value)
-                        )
-                    ));
+                return DataHandler::putTAGByteArray($fPtr, $node->getValue());
             case Tag::TAG_STRING: // String
-                return $this->putTAGString($fPtr, $node->getValue());
+                return DataHandler::putTAGString($fPtr, $node->getValue());
+            case Tag::TAG_INT_ARRAY: // Byte array
+                return DataHandler::putTAGIntArray($fPtr, $node->getValue());
             case Tag::TAG_LIST: // List
                 if ($this->verbose) {
                     trigger_error(
@@ -467,8 +305,8 @@ class Service
                         E_USER_NOTICE
                     );
                 }
-                if (!($this->putTAGByte($fPtr, $node->getPayloadType())
-                    && $this->putTAGInt($fPtr, count($node->getChildren()))
+                if (!(DataHandler::putTAGByte($fPtr, $node->getPayloadType())
+                    && DataHandler::putTAGInt($fPtr, count($node->getChildren()))
                     )) {
                     return false;
                 }
@@ -490,91 +328,8 @@ class Service
                 }
 
                 return true;
-            case Tag::TAG_INT_ARRAY: // Byte array
-                $value = $node->getValue();
-
-                return $this->putTAGInt($fPtr, count($value))
-                    && is_int(fwrite(
-                        $fPtr,
-                        call_user_func_array(
-                            'pack',
-                            array_merge(['N'.count($value)], $value)
-                        )
-                    ));
             case Tag::TAG_END: // End tag
                 return is_int(fwrite($fPtr, "\0"));
         }
-    }
-
-    /**
-     * Write a byte tag to the file.
-     *
-     * @param resource $fPtr
-     * @param byte     $byte
-     *
-     * @return bool
-     */
-    private function putTAGByte($fPtr, $byte)
-    {
-        return is_int(fwrite($fPtr, pack('c', $byte)));
-    }
-
-    /**
-     * Write a string to the file.
-     *
-     * @param resource $fPtr
-     * @param string   $string
-     *
-     * @return bool
-     */
-    private function putTAGString($fPtr, $string)
-    {
-        $value = utf8_encode($string);
-
-        return $this->putTAGShort($fPtr, strlen($value))
-                && is_int(fwrite($fPtr, $value));
-    }
-
-    /**
-     * Write a short int to the file.
-     *
-     * @param resource $fPtr
-     * @param int      $short
-     *
-     * @return bool
-     */
-    private function putTAGShort($fPtr, $short)
-    {
-        return is_int(fwrite($fPtr, pack('n', $this->signedToUnsigned($short, 16))));
-    }
-
-    /**
-     * Write an integer to the file.
-     *
-     * @param resource $fPtr
-     * @param int      $int
-     *
-     * @return bool
-     */
-    private function putTAGInt($fPtr, $int)
-    {
-        return is_int(fwrite($fPtr, pack('N', $this->signedToUnsigned($int, 32))));
-    }
-
-    /**
-     * Convert an unsigned int to signed, if required.
-     *
-     * @param int $value
-     * @param int $size
-     *
-     * @return int
-     */
-    private function signedToUnsigned($value, $size)
-    {
-        if ($value < 0) {
-            $value += pow(2, $size);
-        }
-
-        return $value;
     }
 }
